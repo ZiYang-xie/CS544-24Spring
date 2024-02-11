9b749ed8b4c963300d42dd34eb4a9fcb5ea128cf
venom12138 <1390852413@qq.com>
2024-02-11 17:59:19 -0600

    0211 yjw linear

--- mp1/main.py
+++ mp1/main.py
@@ -3,21 +3,32 @@ import numpy as np
 import numpy.linalg as la
 
 from opt import minimize_with_restart
-from dataset import generate_dataset
+from dataset import generate_linear_regression_dataset
 from utils import plot_func2D
 
 
 class LinearRegression:
     def __init__(self, 
-                 A=np.array([[2, 1], [1, 2]]), 
-                 b=np.array([-5, -3]) ) -> None:
+                A=np.array([[2, 1], [1, 2]]), 
+                b=np.array([-5, -3]) ) -> None:
         self.A = A
         self.b = b
-        self.func = lambda x: 0.5 * x.T @ A @ x + b.T @ x
-        self.grad = lambda x: A @ x + b.T
-        
-    def run(self, method='CG', init_guess=np.array([4,7])):
-        result= minimize_with_restart(self.func, init_guess, method=method, jac=self.grad, tol=1e-2,
+    
+    def func(self, x):
+        obj_func = 0.5 * x.T @ self.A @ x + self.b.T @ x
+        print(f"obj_func: {obj_func}")
+        return obj_func[0]
+    
+    def grad(self, x):
+        grad = self.A @ x + self.b.T
+        print(f"grad: {grad.shape}")
+        return grad
+    
+    def run(self, method='CG', init_guess=None):
+        if init_guess is None:
+            init_guess = np.random.randn(self.A.shape[0],1)
+            
+        result= minimize_with_restart(self.func, init_guess, method=method, jac=None, tol=1e-2,
                             options={
                                 'gtol': 1e-2,
                                 'disp': True,
@@ -26,8 +37,9 @@ class LinearRegression:
         
         plot_func2D(self.func, result['allvecs'], f'{method}.png')
 
-
 if __name__ == '__main__':
-    lr = LinearRegression()
+    x, y, w = generate_linear_regression_dataset(N=100)
+    import pdb; pdb.set_trace()
+    lr = LinearRegression(A=np.dot(x, x.T), b=y)
     lr.run('CG')
     lr.run('BFGS')
