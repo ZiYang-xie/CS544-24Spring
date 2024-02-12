70390a6615b4d6ee20e45e69871f6fc9f5b7b862
venom12138 <1390852413@qq.com>
2024-02-11 21:28:27 -0600

    11

--- mp1/main.py
+++ mp1/main.py
@@ -29,7 +29,7 @@ class LinearRegression:
     def run(self, method='CG', init_guess=None):
         
         if init_guess is None:
-            init_guess = np.random.randn(self.A.shape[0],1)
+            init_guess = np.random.randn(self.A.shape[0])
         
         result= minimize_with_restart(self.func, init_guess, method=method, jac=self.grad, tol=1e-1,
                             options={
@@ -45,12 +45,12 @@ if __name__ == '__main__':
     # x: (N, 2)
     # y: (N, 1)
     # alpha: (N, 1)
-    x, y, w = generate_linear_regression_dataset(N=2000)
+    x, y, w = generate_linear_regression_dataset(N=2000, dim=10)
     lr = LinearRegression(A=np.dot(x, x.T), b=y)
     print(f"------------CG------------")
     import time
     cg_start = time.time()
-    alpha = lr.run('CG', init_guess=np.random.randn(x.shape[0],1))
+    alpha = lr.run('CG', init_guess=np.random.randn(x.shape[0]))
     cg_end = time.time()
     # print(f"alpha: {alpha}")
     w_hat = np.dot(x.T, alpha)
