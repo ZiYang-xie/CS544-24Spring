f13dc2f7e9e9b3b66ae015340bf960aad98b1741
Leo Xie <ziyangxie01@gmail.com>
2024-02-24 22:43:36 -0600

    mp2

--- mp1/main.py
+++ mp1/main.py
@@ -51,6 +51,7 @@ class MLP_fitter:
             layers.append(nn.ReLU())
         self.model = nn.Sequential(*layers, nn.Linear(hidden_dim, output_dim))
 
+
     def func(self, params):
         # Update model weights
         state_dict = {}
@@ -81,7 +82,7 @@ class MLP_fitter:
         params_dict = self.model.state_dict()
         init_params = np.concatenate([params_dict[key].detach().numpy().flatten() for key in params_dict.keys()])
         print("Optimize variable size:", init_params.shape[0])
-        init_params = np.random.randn(init_params.shape[0]) * 10 # Highly Non-linear initial func
+        # init_params = np.random.randn(init_params.shape[0]) * 10 # Highly Non-linear initial func
         print(f"Init loss: {self.func(init_params)}")
 
         print(f"------------{method}------------")
@@ -98,12 +99,12 @@ import memory_profiler
 
 @memory_profiler.profile
 def linear_regression(method='CG'):
-    x, y, w = generate_linear_regression_dataset(N=10000, dim=500)
+    x, y, w = generate_linear_regression_dataset(N=1000, dim=500)
     lr = LinearRegression(A=np.dot(x, x.T), b=y)
     
     print(f"------------{method}------------")
     start = time()
-    alpha = lr.run(method, init_guess=np.random.randn(x.shape[0],1))
+    alpha = lr.run(method, init_guess=np.random.randn(x.shape[0]))
     end = time()
     w_hat = np.dot(x.T, alpha)
     print(f"Time Usage: {end-start}, Estimated weights: {w_hat}, True weights: {w}")
@@ -119,42 +120,42 @@ def mlp_fitting(method='CG'):
             'time': [],
             'iterations': [],
         }
-        for _ in range(5):
-            mlp = MLP_fitter(X, Y, input_dim=1, hidden_dim=16, output_dim=1, layer_num=layer)
+        # for _ in range(5):
+        mlp = MLP_fitter(X, Y, input_dim=1, hidden_dim=16, output_dim=1, layer_num=layer)
 
-            print("\n"+"="*40)
-            start = time()
-            results = mlp.run(method)
-            end = time()
-            print(f"Time Usage: {end-start}")
+        print("\n"+"="*40)
+        start = time()
+        results = mlp.run(method)
+        end = time()
+        print(f"Time Usage: {end-start}")
 
-            # Test the model
-            param = results.x
-            state_dict = {}
-            for key in mlp.model.state_dict().keys():
-                state_dict[key] = torch.tensor(param[:mlp.model.state_dict()[key].numel()]).reshape(mlp.model.state_dict()[key].shape)
-                param = param[mlp.model.state_dict()[key].numel():]
-            mlp.model.load_state_dict(state_dict)
-            error = F.mse_loss(mlp.model(mlp.X), mlp.Y, reduction='mean').item()
+        # Test the model
+        param = results.x
+        state_dict = {}
+        for key in mlp.model.state_dict().keys():
+            state_dict[key] = torch.tensor(param[:mlp.model.state_dict()[key].numel()]).reshape(mlp.model.state_dict()[key].shape)
+            param = param[mlp.model.state_dict()[key].numel():]
+        mlp.model.load_state_dict(state_dict)
+        error = F.mse_loss(mlp.model(mlp.X), mlp.Y, reduction='mean').item()
 
-            result_dict['error'].append(error)
-            result_dict['time'].append(end-start)
-            result_dict['iterations'].append(results.nit)
+        result_dict['error'].append(error)
+        result_dict['time'].append(end-start)
+        result_dict['iterations'].append(results.nit)
 
-            # print(f"MSE Error: {error}")
-            # test_X = np.linspace(-5, 5, 10000).reshape(-1, 1)
-            # print(f"x: {test_X.shape}")
-            # visulize(mlp.model, tgt_func, torch.FloatTensor(test_X), method)
-            print("="*40 + "\n")
+        print(f"MSE Error: {error}")
+        test_X = np.linspace(-3, 3, 10000).reshape(-1, 1)
+        print(f"x: {test_X.shape}")
+        visulize(mlp.model, tgt_func, torch.FloatTensor(test_X), f'{method}_layer{layer}')
+        print("="*40 + "\n")
         
         with open(f"results/final_res.txt", 'a') as f:
             f.write(f"Layer: {layer}, Method: {method}, Error: {np.mean(result_dict['error'])}, Time: {np.mean(result_dict['time'])}, Iterations: {np.mean(result_dict['iterations'])}\n")
 
 if __name__ == '__main__':
     # linear_regression('CG')
-    linear_regression('BFGS')
-    # mlp_fitting('CG')
-    # mlp_fitting('BFGS')
+    # linear_regression('BFGS')
+    mlp_fitting('CG')
+    mlp_fitting('BFGS')
 
 
     
