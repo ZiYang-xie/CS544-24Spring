b3fcd0508373dcfe5581ca192ffedce5effeb4a6
Leo Xie <ziyangxie01@gmail.com>
2024-05-03 23:30:08 -0500

    Add alpha extension

--- mp4/alpha_extension.py
+++ mp4/alpha_extension.py
@@ -23,7 +23,7 @@ def find_nearest_value_dist(matrix, value):
     distances = np.round(distances).astype(int)
     return distances
 
-def create_graph(img, mask, fg_prob, bg_prob):
+def create_graph(img, mask, fg_prob, bg_prob, lam=1):
     """
     Create a graph for the min-cut segmentation.
     """
@@ -35,17 +35,15 @@ def create_graph(img, mask, fg_prob, bg_prob):
     bg_dist = find_nearest_value_dist(mask, 0)
 
     H, W = img.shape[:2]
-    fg_dist = fg_dist / max(H, W)
-    bg_dist = bg_dist / max(H, W)
+    fg_dist = 1 - fg_dist / max(H, W)
+    bg_dist = 1 - bg_dist / max(H, W)
 
     # Add edges between pixels and source/sink
     for y in range(img.shape[0]):
         for x in range(img.shape[1]):
             idx = indices[y, x]
-            
-            graph.add_edge('source', idx, capacity=fg_prob[y,x]+3*fg_dist[y,x]) # 100
-            # elif mask[y, x] == 0:
-            graph.add_edge(idx, 'sink', capacity=bg_prob[y,x]+3*bg_dist[y,x]) # 100
+            graph.add_edge('source', idx, capacity=fg_prob[y,x] + lam*fg_dist[y,x])
+            graph.add_edge(idx, 'sink', capacity=bg_prob[y,x] + lam*bg_dist[y,x])
 
             if x > 0:  # Left pixel
                 left_idx = indices[y, x - 1]
@@ -58,84 +56,77 @@ def create_graph(img, mask, fg_prob, bg_prob):
                 graph.add_edge(idx, up_idx, capacity=weight)
     return graph
 
-def segment_image(image, mask, gmm_model, round=10):
+def segment_image(image, masks, round=5):
     img = image
+    tol = 1e-7
+    arc_cut_value = -1e-9
 
-    tol = 5
-    arc_cut_value = 0
-    # Create a graph
-    for _ in range(round):
-        logger.info('Round: {}'.format(_))
+    fg_gmm = GaussianMixture(n_components=2, random_state=0)
+    bg_gmm = GaussianMixture(n_components=2, random_state=0)
 
-        # Compute the probability of each pixel belonging to the foreground and background
-        data_eval = img.reshape(-1, 3) / 255.
-        fg_prob, bg_prob = gmm_model['fg'].score_samples(data_eval), gmm_model['bg'].score_samples(data_eval)
-        # import pdb; pdb.set_trace()
-        # fg_prob = np.exp(fg_prob)*1/256
-        # bg_prob = np.exp(bg_prob)*1/256
-        # import pdb; pdb.set_trace()
-        fg_prob =(fg_prob - fg_prob.min()) / (fg_prob.max()-fg_prob.min())
-        bg_prob =(bg_prob - bg_prob.min()) / (bg_prob.max()-bg_prob.min())
-        fg_prob = -np.log(fg_prob + 1e-6)
-        bg_prob = -np.log(bg_prob + 1e-6)
-        # import pdb; pdb.set_trace()
-        # fg_prob, bg_prob = np.exp(fg_prob), np.exp(bg_prob)
-        # mask = fg_prob > bg_prob
-        # mask = mask.reshape(img.shape[:2]).astype(np.uint8)
-        
-        fg_prob = fg_prob.reshape(img.shape[:2])
-        bg_prob = bg_prob.reshape(img.shape[:2])
-        
-        # Create a graph for the min-cut segmentation
-        graph = create_graph(img, mask, fg_prob, bg_prob)
-        cut_value, partition = nx.minimum_cut(graph, 'source', 'sink')
-        reachable, non_reachable = partition
-        logger.info(f"Cut value: {cut_value}")
+    for index in range(masks.shape[0]):
+        source_mask = masks[index]
+        for _ in range(round): 
+            logger.info('Round: {}'.format(_))
 
-        # Update the mask
-        mask = np.zeros_like(mask)
-        for segment in non_reachable:
-            if segment != 'sink':
-                y, x = np.divmod(segment, img.shape[1])
-                mask[y, x] = 1
+            fg_data = image[source_mask] / 255.
+            bg_data = image[~source_mask] / 255.
+            fg_gmm.fit(fg_data)
+            bg_gmm.fit(bg_data)
 
-        # Update the GMM model
-        mask = mask.astype(bool)
-        fg_data = img[mask] / 255.
-        bg_data = img[~mask] / 255.
-        gmm_model['fg'].fit(fg_data)
-        gmm_model['bg'].fit(bg_data)
+            # Compute the probability of each pixel belonging to the foreground and background
+            data_eval = img.reshape(-1, 3) / 255.
+            fg_prob, bg_prob = fg_gmm.score_samples(data_eval), bg_gmm.score_samples(data_eval)
+            
+            fg_prob = fg_prob.reshape(img.shape[:2])
+            bg_prob = bg_prob.reshape(img.shape[:2])
+            fg_prob = (fg_prob - np.min(fg_prob)) / (np.max(fg_prob) - np.min(fg_prob))
+            bg_prob = (bg_prob - np.min(bg_prob)) / (np.max(bg_prob) - np.min(bg_prob))
+            fg_prob, bg_prob = np.exp(fg_prob), np.exp(bg_prob)
 
-        if abs(cut_value - arc_cut_value) < tol:
-            break
-        arc_cut_value = cut_value
+            # Create a graph for the min-cut segmentation
+            graph = create_graph(img, source_mask, fg_prob, bg_prob)
+            cut_value, partition = nx.minimum_cut(graph, 'source', 'sink')
+            reachable, non_reachable = partition
+            logger.info(f"Cut value: {cut_value}")
+
+            # Update the mask
+            source_mask = np.zeros_like(source_mask)
+            for segment in reachable:
+                if segment != 'source':
+                    y, x = np.divmod(segment, img.shape[1])
+                    source_mask[y, x] = 1
+
+            # Update the GMM model
+            source_mask = source_mask.astype(bool)
+            fg_data = img[source_mask] / 255.
+            bg_data = img[~source_mask] / 255.
+            fg_gmm.fit(fg_data)
+            bg_gmm.fit(bg_data)
+
+            if abs(cut_value - arc_cut_value) < tol:
+                break
+            arc_cut_value = cut_value
 
     logger.info('Segmentation done.')
     return mask
 
 def process(input, max_size=256):
     ori_image = input['background'][:,:,:3]
-    mask = input['layers'][0][:,:,-1]>0
+    masks = np.array(input['layers'])[...,-1]>0
 
-    fg_gmm = GaussianMixture(n_components=1, random_state=0)
-    bg_gmm = GaussianMixture(n_components=1, random_state=0)
-    fg_data = ori_image[mask] / 255.
-    bg_data = ori_image[~mask] / 255.
-    fg_gmm.fit(fg_data)
-    bg_gmm.fit(bg_data)
-
-    gmm_model = {
-        'fg': fg_gmm,
-        'bg': bg_gmm
-    }
-    
     # Resize the image if it is too large
     if max(ori_image.shape[:2]) > max_size:
         scale = max_size / max(ori_image.shape[:2])
         image = cv2.resize(ori_image, (0, 0), fx=scale, fy=scale)
-        mask = cv2.resize(mask.astype(np.uint8), (0, 0), fx=scale, fy=scale) > 0
-    
-    refined_mask = segment_image(image, mask, gmm_model=gmm_model)
+        resized_masks = []
+        for mask in masks:
+            resized_masks.append(cv2.resize(mask.astype(np.uint8), (0, 0), fx=scale, fy=scale) > 0)
+        masks = np.array(resized_masks)
+
+    masks = masks.astype(np.uint8)
+    refined_mask = segment_image(image, masks)
+    import pdb; pdb.set_trace()
     mask = cv2.resize(refined_mask.astype(np.uint8), ori_image.shape[:2][::-1]) == 0
     segmented_image = input['background'].copy()
     segmented_image[mask] = np.concatenate([segmented_image[mask][:,:3], 
